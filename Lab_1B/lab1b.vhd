f41d5b7e4a0edbc4848433b1e2d6ff2e6cd519a6
engineeredkaos <33166233+engineeredkaos@users.noreply.github.com>
2020-09-02 18:34:18 +0000

    Add files via upload

---
+++ Lab_1B/lab1b.vhd
@@ -0,0 +1,29 @@
+----------------------------------------------------------------------
+-- Digital Design 101 Lab Assignment 1 - Z20
+-- LFDetector Behavioral Model
+----------------------------------------------------------------------
+-- Student First Name : Cristian
+-- Student Last Name : Magana
+-- Student ID : 37578770
+----------------------------------------------------------------------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+ENTITY LFDetector_behav IS
+   PORT (Fuel3, Fuel2, Fuel1, Fuel0: IN std_logic;
+         FuelWarningLight: OUT std_logic);
+END LFDetector_behav;
+
+ARCHITECTURE Behavior OF LFDetector_behav IS
+
+-- DO NOT modify any signals, ports, or entities above this line
+-- add your code below this line
+-- put your code in a PROCESS
+-- use AND/OR/NOT keywords for behavioral (Boolean) function
+BEGIN
+    PROCESS(Fuel2, Fuel3)
+    BEGIN
+        FuelWarningLight <= NOT Fuel2 AND NOT Fuel3 AFTER 4.4 NS;
+    END PROCESS;
+END Behavior;
